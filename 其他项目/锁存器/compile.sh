036b267fc5df53ca2ffb6f90d75ac2bad45b5665
luogantt <290328566@qq.com>
2022-05-29 22:49:24 +0800

    锁存器

---
+++ 其他项目/锁存器/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "开始编译"
+
+iverilog fn_sw.v stimulus_tb.v -o fn
+
+#./invet
+
+echo "编译完成"
+vvp -n fn -lxt2
+echo "生成波形文件"
+cp test.vcd wave.lxt
+echo "打开波形文件"
+gtkwave wave.lxt
+
---
+++ 其他项目/锁存器/fn_sw.v
@@ -0,0 +1,23 @@
+//2022-05-15  罗干
+//二选一逻辑设计
+`timescale 1ns/10ps 
+
+
+module fn_sw(a,b,sel,y);
+
+
+input  a,b,sel;
+output y;
+
+// assign y=sel?(a^b):(a&b);
+
+reg y;
+always@(a or b or sel ) begin 
+
+   if (sel==1) begin 
+       y<=a;
+       end 
+
+    end
+
+endmodule 
